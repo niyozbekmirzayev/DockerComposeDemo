c8c860608c3131c138cd441ce2d4426c40a3e070
Niyozbek Mirzayev <n.mirzayev@baik.uz>
2023-01-30 00:02:04 +0500

    configured for https

--- ColorAPI/Program.cs
+++ ColorAPI/Program.cs
@@ -26,7 +26,7 @@ namespace ColorAPI
                 app.UseSwaggerUI();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
